9c46f273639447b0974cd9db6320eab4305a02db
Ahmet Uyar <4hmetuyar@gmail.com>
2017-10-16 21:42:13 +0300

    Service Arayüzlerinin Oluşturulması.

---
+++ Abc.Northwind.Business/Apstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Northwind.Entites.Concreate;
+
+namespace Abc.Northwind.Business.Apstract
+{
+   public interface IProductService 
+    {
+        //Backendin çıkış noktasıdır. Repository deseni veri erişim katmanında kullanılması gerekir.
+
+        List<Product> GetAll();
+        List<Product> GetByCategory(int categoryId);
+        void Add(Product product);
+        void Upadte(Product product);
+        void Delete(int productId);
+    }
+}
