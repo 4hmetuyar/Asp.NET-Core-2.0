323ec9b65907deca758e17c272f69eacc3a7219b
Ahmet Uyar <4hmetuyar@gmail.com>
2017-10-16 21:53:32 +0300

    Manager Sınıflar yazıldı.

---
+++ Abc.Northwind.Business/Concreate/CategoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Abc.Northwind.Business.Apstract;
+using Abc.Northwind.DataAccess.Apstract;
+using Abc.Northwind.Entites.Concreate;
+
+namespace Abc.Northwind.Business.Concreate
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategorytDal _categorytDal;
+
+        public CategoryManager(ICategorytDal categorytDal)
+        {
+            _categorytDal = categorytDal;
+        }
+
+        public List<Category> GetAll()
+        {
+           return _categorytDal.GetList();
+        }
+    }
+}
---
+++ Abc.Northwind.Business/Concreate/ProductManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Northwind.Business.Apstract;
+using Abc.Northwind.DataAccess.Apstract;
+using Abc.Northwind.DataAccess.Concreate.EntityFramework;
+using Abc.Northwind.Entites.Concreate;
+
+namespace Abc.Northwind.Business.Concreate
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+
+        public List<Product> GetAll()
+        {
+            //EfProductDal productDal  = new EfProductDal(); //Bu şekilde çalışır fakat SOLID standartlarına göre üst katman alt katmanı newleyemez. 
+            ////Bu şekilde Business entity Framawork tamamen bağımlı hale gelmiş olur.
+            //return productDal.GetList();
+
+
+            return _productDal.GetList();
+        }
+
+        public List<Product> GetByCategory(int categoryId)
+        {
+            return _productDal.GetList(x => x.CategoryId == categoryId);
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Upadte(Product product)
+        {
+            _productDal.Update(product);
+        }
+
+        public void Delete(int productId)
+        {
+            _productDal.Delete(new Product { ProductId = productId });
+        }
+    }
+}
