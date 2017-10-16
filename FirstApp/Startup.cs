481a3b466b0c8a02de8524cb5923ec6f78dde30e
Ahmet Uyar <4hmetuyar@gmail.com>
2017-10-16 23:36:37 +0300

    Custom Middeware yazıldı.

--- FirstApp/Startup.cs
+++ FirstApp/Startup.cs
@@ -6,6 +6,7 @@ using Abc.Northwind.Business.Apstract;
 using Abc.Northwind.Business.Concreate;
 using Abc.Northwind.DataAccess.Apstract;
 using Abc.Northwind.DataAccess.Concreate.EntityFramework;
+using Abc.Northwind.WebUI.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -53,7 +54,8 @@ namespace FirstApp
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles(); //sabit modülleri wwwroot içinde alyans vermek için kullandýk.
+            app.UseNodeModules(env.ContentRootPath);
 
             app.UseMvc(routes =>
             {
