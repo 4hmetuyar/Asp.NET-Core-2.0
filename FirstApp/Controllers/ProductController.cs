864681fc93c9aa8b707928ff8de3642587faec0d
Ahmet Uyar <4hmetuyar@gmail.com>
2017-10-19 22:55:56 +0300

    SessionExtensionMethods Eklendi.

--- FirstApp/Controllers/ProductController.cs
+++ FirstApp/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Abc.Northwind.Business.Apstract;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -28,6 +29,12 @@ namespace Abc.Northwind.WebUI.Controllers
 
         public IActionResult Create()
         {
+            HttpContext.Session.SetString("Hazırlayan","Ahmet");
+            HttpContext.Session.SetInt32("Deger",3);
+
+            HttpContext.Session.GetInt32("Deger");
+            HttpContext.Session.GetString("Hazırlayan");
+
             return View();
         }
     }
